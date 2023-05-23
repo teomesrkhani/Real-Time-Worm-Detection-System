4c3a1037a3a9444d04a8eb1196a9714af89142e2
Teo Mesrkhani <teomesrkhani@gmail.com>
2023-05-23 11:09:45 -0400

    in-lab work

--- WormDetection.py
+++ WormDetection.py
@@ -8,6 +8,14 @@ import serial
 #### import timeit
 
 is_delaying = False
+
+def delay_seconds(time):
+  global is_delaying
+
+  if len(results.xyxy[0]) == 8:
+    sleep(2)
+    print("time over")
+
 def perform_action():
     global is_delaying
     #print(f"{confidence}, {len(results.xyxy[0])} {results.xyxy[0]} yes")
@@ -35,7 +43,7 @@ def perform_action():
 #cap = cv2.VideoCapture(0 + cv2.CAP_DSHOW)
 
 # How to capture data from video file
-cap = cv2.VideoCapture("CFL3.mp4") # Capture from file
+cap = cv2.VideoCapture("Adult worm 2.mp4") # Capture from file
 
 #num_frames_to_record = 100
 num_frames_to_record = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) # record all frames from video (if using file)
@@ -52,7 +60,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 torch.set_printoptions(sci_mode=False)
 
 # Load our model
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='worm_best_050123.pt', force_reload=True)
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='worm_model_0523.pt', force_reload=True)
 model.to(device)
 
 window_width = 800
@@ -79,6 +87,15 @@ while num_frames_to_record > 0:
     red_colour = (0, 0, 255)
     thickness = 2
 
+    
+    #print(f"\n{filter(results.pandas().xyxy[0].to_dict()['name'], 'larva_worm')}\n")
+    a = (results.pandas().xyxy[0].to_dict()['name'])
+
+    #print((a))
+
+    b = list((results.pandas().xyxy[0].to_dict()['name']).values())
+    print(f"name:{b}\ncount: {b.count('larva_worm')}")
+    
     # Draw a rectangle around every detection in the results
     for result in results.xyxy[0]:
       x1 = result[0]
@@ -96,7 +113,10 @@ while num_frames_to_record > 0:
       # Display confidence value in real-time
       confidence_to_text = f"{confidence: .2f}"
       cv2.putText(frame ,confidence_to_text, start_point, cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 2)
-      
+    
+      #print(result)
+
+      """
       if confidence >= 0.9 and len(results.xyxy[0]) == 8:
       #if confidence >= 0.9 and len(results.xyxy[0]) == 5:
         #sleep(2)
@@ -104,7 +124,7 @@ while num_frames_to_record > 0:
           #serial1.write(b'\xaa')
           #serial.write(b'\xac')
 
-        """
+
         start = (time()*1000)
         sleep(0.05)
         end=(time()*1000)
@@ -112,9 +132,10 @@ while num_frames_to_record > 0:
         start = time()*1000
         end=time()*1000
         print(f"time 2: {end - start}")
-        """
+
         if not is_delaying:
           is_delaying = True
+          #Timer(delay_seconds(2)
           Timer(4, perform_action).start()
           print("started")
           #for i in results.xyxy[0]:
@@ -131,6 +152,8 @@ while num_frames_to_record > 0:
           
         if is_delaying:
           continue
+
+        """
 
 
     cv2.imshow('Worm Detection', frame)
